9c68e51c61d54f3e1a964fe61a418fc48e77d791
ctsiaousis <ctsiaous@gmail.com>
2020-04-22 00:26:06 +0300

    merge the big change to upstream

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-picom --experimental-backends && killall -SIGUSR1 picom &&
-picom --experimental-backends &
 $HOME/.local/bin/processgraph &
-urxvtd &
-mpd &
-conky && killall -SIGUSR1 conky && 
-conky &
 setxkbmap us,gr -option grp:alt_caps_toggle,grp_led:scroll &
+#conky && killall -SIGUSR1 conky && conky &
--- .config/awesome/lock.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-image=$(date +%s).png
-
-scrot /tmp/$image && 
-convert /tmp/$image -blur 0x5 /tmp/screenshotblur.png && 
-i3lock -i /tmp/screenshotblur.png &&
-rm -f /tmp/$image /tmp/screenshotblur.png
